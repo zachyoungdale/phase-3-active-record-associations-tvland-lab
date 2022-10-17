f3cf8209eb8c2c5caf3266894ffe526c5719a36e
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-17 17:20:41 -0400

    commit tree c6ff723d2907fdbfbd111c683a80825667062996

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
+
+    def actors_list 
+        self.characters.map do |character|
+            "#{character.actor.first_name} #{character.actor.last_name}"
+        end
+    end
   
 end
